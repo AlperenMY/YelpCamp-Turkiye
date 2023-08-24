fed6e28322a7a3b8eae154c438130f6c87d90be9
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-08-24 16:53:54 +0300

    Campground Model Basics

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+const { Campground } = require("./models/campground");
+
+const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelp-camp")
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => {
+    console.log("MongoDB Eror: ", err);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/newcampground", async (req, res) => {
+  const camp = new Campground({ title: "My Backyard" });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
