58658625b579ad8a9283072ca01738e6bd8702fb
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-09-04 22:51:44 +0300

    Campground delete

--- app.js
+++ app.js
@@ -52,6 +52,12 @@ app.put("/campgrounds/:id", async (req, res) => {
   res.redirect(`/campgrounds/${id}`);
 });
 
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
+
 app.get("/campgrounds/:id/edit", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/edit", { campground });
