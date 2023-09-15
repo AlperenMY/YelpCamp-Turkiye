cef7b16bc45da1544219f300a9c82589bf376d96
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-09-15 17:01:51 +0300

    Added review form

--- models/review.js
+++ models/review.js
@@ -3,10 +3,7 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
   body: String,
-  rating: {
-    type: Number,
-    enum: [1, 2, 3, 4, 5],
-  },
+  rating: Number,
 });
 
 exports.Review = mongoose.model("Review", reviewSchema);
