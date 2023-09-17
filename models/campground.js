24c866de4c7251a9b45b58d92a054e03afc2454d
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-09-17 22:10:47 +0300

    added campground delete middleware

--- models/campground.js
+++ models/campground.js
@@ -1,16 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema({
+const { Review } = require("./review");
+
+const campgroundSchema = new Schema({
   title: String,
   price: Number,
   description: String,
   location: String,
   image: String,
-  reviews: [{
-    type: Schema.Types.ObjectId,
-    ref: "Review"
-  }]
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
-exports.Campground = mongoose.model("Campground", CampgroundSchema);
+campgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({ _id: { $in: doc.reviews } });
+  }
+});
+
+exports.Campground = mongoose.model("Campground", campgroundSchema);
