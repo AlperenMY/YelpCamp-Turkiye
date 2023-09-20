f3614e4457bb05539b31de8dd767353757d8b15b
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-09-20 15:14:16 +0300

    initial configuration for session

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
 
 const { AppError } = require("./utils/AppError");
 const { reviewsRouter } = require("./routes/reviews");
 const { campgroundsRouter } = require("./routes/campgrounds");
 
 const app = express();
+const sessionConfig = {
+  secret: "mylittlesecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+};
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
@@ -27,6 +38,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
+app.use(session(sessionConfig));
 app.use("/campgrounds", campgroundsRouter);
 app.use("/campgrounds/:id/reviews", reviewsRouter);
 
