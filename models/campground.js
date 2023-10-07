3ec2133806f2a10bb7f9fead2e1ca73d3d90ca4f
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-10-07 14:15:31 +0300

    added thumbnail version of images

--- models/campground.js
+++ models/campground.js
@@ -3,17 +3,21 @@ const Schema = mongoose.Schema;
 
 const { Review } = require("./review");
 
+const imageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+imageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("upload/", "upload/w_200/");
+});
+
 const campgroundSchema = new Schema({
   title: String,
   price: Number,
   description: String,
   location: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [imageSchema],
   reviews: [
     {
       type: Schema.Types.ObjectId,
