6c1fbf8fabd800a8f767ee8bcc677a20c39c75a3
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-10-09 00:18:51 +0300

    center map on the campground

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,6 +2,9 @@ mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 13, // starting zoom
 });
+const marker = new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .addTo(map);
