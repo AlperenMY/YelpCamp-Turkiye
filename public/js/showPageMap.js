6d17711e59e6d310cca8366a547bca7a526ddfba
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-10-09 18:37:06 +0300

    added map popup and some style

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,10 +1,14 @@
 mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  style: "mapbox://styles/mapbox/light-v11", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 13, // starting zoom
 });
+const popup = new mapboxgl.Popup({ offset: 25, closeButton: false }).setHTML(
+  `<h6>${campground.title}</h6>`
+);
 const marker = new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
+  .setPopup(popup)
   .addTo(map);
