182f0b624b07aeba5afbf7e182dc041c2d3098b7
Alperen M. Yilmaz <724yilmaz@gmail.com>
2023-10-13 20:27:11 +0300

    added sanitizing HTML within Joi validation

--- joiSchemas.js
+++ joiSchemas.js
@@ -1,11 +1,33 @@
-const Joi = require("joi");
+const baseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not contain HTML tags",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value) return helpers.error("string.escapeHTML");
+        return clean;
+      },
+    },
+  },
+});
+const Joi = baseJoi.extend(extension);
 
 exports.joiCampgroundSchema = Joi.object({
   campground: Joi.object({
-    title: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
     price: Joi.number().required().min(0),
-    description: Joi.string().required(),
-    location: Joi.string().required(),
+    description: Joi.string().required().escapeHTML(),
+    location: Joi.string().required().escapeHTML(),
     // image: Joi.string().required(),
   }).required(),
   deletedImages: Joi.array(),
@@ -13,7 +35,7 @@ exports.joiCampgroundSchema = Joi.object({
 
 exports.joiReviewSchema = Joi.object({
   review: Joi.object({
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
     rating: Joi.number().integer().min(1).max(5).required(),
   }).required(),
 });
